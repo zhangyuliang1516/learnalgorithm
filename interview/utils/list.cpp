36a488bec8483098d7fce9e62194062393a54a4b
Yuliang Zhang <yuliang.zhang@focus-ebeam.com>
2024-06-17 15:19:55 +0800

    Add list to utils
    Change-Id: I7222b3cf4c68b22bb2cd83ffe40437d0a7870e79

--- interview/utils/list.cpp
+++ interview/utils/list.cpp
@@ -1,5 +1,89 @@
 #include "list.h"
 
-List::List()
+#include <iostream>
+
+using namespace std;
+
+ListNode *createListNode(int value)
 {
+    ListNode *node = new ListNode;
+    node->m_value = value;
+    node->m_next = nullptr;
+    return node;
+}
+
+void connectListNode(ListNode *current, ListNode *next)
+{
+    if (!current) {
+        cout << "Error to connect tow nodes" << endl;
+        return;
+    }
+    current->m_next = next;
+}
+
+void printListNode(ListNode *current)
+{
+    if (current)
+        cout << "The key in node is: " << current->m_value << endl;
+    else
+        cout << "The node is nullptr";
+}
+
+void printList(ListNode *head)
+{
+    cout << "Start printing list" << endl;
+    ListNode *next = head;
+    while (next) {
+        cout << next->m_value << "\t";
+        next = next->m_next;
+    }
+    cout << "Print list finished" << endl;
+}
+
+void destroyList(ListNode *head)
+{
+    ListNode *next = head;
+    while (head) {
+        head = head->m_next;
+        delete next;
+        next = head;
+    }
+}
+
+void addToTail(ListNode **head, int value)
+{
+    ListNode *node = new ListNode;
+    node->m_value = value;
+    node->m_next = nullptr;
+    if (*head) {
+        ListNode *next = *head;
+        while (next->m_next)
+            next = next->m_next;
+        next->m_next = node;
+    } else {
+        *head = node;
+    }
+}
+
+void removeNode(ListNode **head, int value)
+{
+    if (head == nullptr || *head == nullptr)
+        return;
+    ListNode *toBeDeleted = nullptr;
+    if ((*head)->m_value == value) {
+        toBeDeleted = *head;
+        *head = (*head)->m_next;
+    } else {
+        ListNode *node = *head;
+        while (node->m_next && node->m_next->m_value != value)
+            node = node->m_next;
+        if (node->m_next && node->m_next->m_value == value) {
+            toBeDeleted = node->m_next;
+            node->m_next = node->m_next->m_next;
+        }
+    }
+    if (toBeDeleted) {
+        delete toBeDeleted;
+        toBeDeleted = nullptr;
+    }
 }
--- interview/utils/list.h
+++ interview/utils/list.h
@@ -9,3 +9,7 @@ struct ListNode
 _declspec(dllexport) ListNode *createListNode(int value);
 _declspec(dllexport) void connectListNode(ListNode *current, ListNode *next);
 _declspec(dllexport) void printListNode(ListNode *current);
+_declspec(dllexport) void printList(ListNode *head);
+_declspec(dllexport) void destroyList(ListNode *head);
+_declspec(dllexport) void addToTail(ListNode **head, int value);
+_declspec(dllexport) void removeNode(ListNode **head, int value);
